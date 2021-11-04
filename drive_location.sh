18624063ef14b371534702bc89cb1465bcf59455
emilkurian <emilkurian@gmail.com>
2021-11-04 11:16:01 -0700

    Working, supports multi-expander systems

--- drive_location.sh
+++ drive_location.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-##### Expander and Drive List
-
-EXPANDER="$(lsscsi -g | grep encl | awk '{print $4" "7}')"
-DRIVES="$(lsscsi -tg | grep sd | awk '{print $3" "$4}')"
-
-
 ##### Drive Logic
 
-header="\n %-10s %8s %10s\n"
+header="\n %-8s %8s %12s\n"
+
+lsscsi -g | grep encl | awk '{print $4" "$7}' > expanders.txt
+lsscsi -tg | grep sd | awk '{print $3" "$4}' > drives.txt
 
 
 printf "$header" "DRIVE" "SLOT" "EXPANDER" 
 
-file1 = $EXPANDER
-while IFS=" " read -r b1 b2
+# Rectify Names to Drive slots/Expanders
+
+cat expanders.txt | while IFS=" " read -r b1 b2
 do 
-    file2=$DRIVES
-    while IFS=" " read -r f1 f2
+    cat drives.txt | while IFS=" " read -r f1 f2
     do 
-        SASADDR = ${f1:6}
-        if grep -q >>> ${sg_ses --sas_addr="$SASADDR" "$b2"}; then
+
+        SASADDR=${f1:6}
+        if sg_ses --sas_addr="$SASADDR" "$b2"| grep -q match; then
             :
-        else:
-            SLOT=${sg_ses --sas_addr="$SASADDR" "$b2" | grep Slot | awk '{print $1}'}
-            printf "$header" "$f2" "$SLOT" "$B1" 
-        
-    done <"$file2"
+        else
+            SLOT="$(sg_ses --sas_addr="$SASADDR" "$b2" | grep Slot | awk '{print $1}')"
+            printf "$header" "$f2" "$SLOT" "$b1" 
+	fi        
+    done
 
+done
 
-done <"$file1"
+# Cleanup
+
+rm expanders.txt
+rm drives.txt
 
 exit 0 
+
